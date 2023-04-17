07273e675d03ab81035f2ac5377a62b2ef57fc75
DeadWinnerv <vitwinner@ya.ru>
2023-04-17 21:57:54 +0300

    chart js and orders inited

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { MatIconModule } from '@angular/material/icon';
+import { NgChartsModule } from 'ng2-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,7 +12,7 @@ import { InventoryComponent } from './components/inventory/inventory.component';
 import { StoragesComponent } from './components/storages/storages.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { OrdersComponent } from './orders/orders.component';
+import { OrdersComponent } from './components/orders/orders.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,7 @@ import { OrdersComponent } from './orders/orders.component';
     InventoryComponent,
     StoragesComponent,
     ProfileComponent,
-    OrdersComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule,
@@ -29,6 +30,7 @@ import { OrdersComponent } from './orders/orders.component';
     HttpClientModule,
     BrowserAnimationsModule,
     MatIconModule,
+    NgChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
