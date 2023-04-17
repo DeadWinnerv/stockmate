f2ac8fdbaf3a4a0f3affffafa123294eed857c73
DeadWinnerv <vitwinner@ya.ru>
2023-04-17 19:23:27 +0300

    basic layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { InventoryComponent } from './components/inventory/inventory.component';
+import { StoragesComponent } from './components/storages/storages.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: DashboardComponent},
+  {path: 'inventory', component: InventoryComponent},
+  {path: 'storages', component: StoragesComponent},
+  {path: 'profile', component: ProfileComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { InventoryComponent } from './components/inventory/inventory.component';
+import { StoragesComponent } from './components/storages/storages.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideMenuComponent,
+    DashboardComponent,
+    InventoryComponent,
+    StoragesComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
