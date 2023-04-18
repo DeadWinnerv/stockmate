e06bba3a6f272dbb6af21d1934a0b92e8a59efed
DeadWinnerv <vitwinner@ya.ru>
2023-04-18 20:25:48 +0300

    login page animation added

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  animations: [
+    trigger('slideIn', [
+      transition(':enter', [
+        style({ transform: 'translate(-50%,200%)' }),
+        animate('0.8s ease-out', style({ transform: 'translate(-50%,-50%)' })),
+      ]),
+    ]),
+  ],
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
