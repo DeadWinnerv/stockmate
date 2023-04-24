ed58827d3783c731773a517944f8412a48879571
DeadWinnerv <vitwinner@ya.ru>
2023-04-24 21:31:32 +0300

    profile fucking merge bux fix

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -20,18 +20,48 @@ type TchangeAvatarButton = 'active' | null;
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent implements OnInit, ViewChild {
+export class ProfileComponent implements OnInit {
   @ViewChild('input') inputRef: ElementRef;
   changeAvatarButton: TchangeAvatarButton = null;
   user: USER;
   aSub: Subscription;
-  constructor(private auth: AuthService) {
-
+  avatar: File;
+  uploadError: any;
+  isErrorDisplay: boolean = false;
+  constructor(private auth: AuthService) {}
+  checkExtension(): boolean {
+    let ext: any = this.avatar.name.split('.').pop();
+    let ext_arr = ['jpeg', 'png', 'jpg', 'gif', 'svg'];
+    if (!ext_arr.includes(ext)) return false;
+    else return true;
   }
-  ngOnInit(): void{
+  onFileUpload(event: any) {
+    let obs$;
+    const file = event.target.files[0];
+    this.avatar = file;
+    obs$ = this.auth.uploadAvatar(this.avatar);
+    if (!this.checkExtension()) {
+      this.uploadError = 'Неверный тип файла';
+      this.isErrorDisplay = true;
+    } else {
+      obs$.subscribe({
+        next: (res) => {
+          this.user.avatar = res.avatar;
+        },
+        error: (err: any) => {
+          this.uploadError = err.error.message;
+          this.isErrorDisplay = true;
+        },
+      });
+    }
+  }
+  handleClickAvatar(): void {
+    this.inputRef.nativeElement.click();
+  }
+  ngOnInit(): void {
     this.aSub = this.auth.getMe().subscribe({
       next: () => {
-          this.user = this.auth.user;
+        this.user = this.auth.user;
       },
       error: (err: any) => {
         console.log(err);
