e117bd87977793c4047f5e949dabccfcb7df23c5
DeadWinnerv <vitwinner@ya.ru>
2023-04-28 14:57:50 +0300

    storage filter by select bug fixed

--- src/app/components/inventory/inventory.component.ts
+++ src/app/components/inventory/inventory.component.ts
@@ -1,53 +1,52 @@
 import { Component, ViewChild, AfterViewInit } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator'
-import { MatTable, MatTableDataSource } from '@angular/material/table'
-import { MatSort, Sort } from '@angular/material/sort'
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
+import { MatSort, Sort } from '@angular/material/sort';
 import { LiveAnnouncer } from '@angular/cdk/a11y';
 
 export interface inventoryData {
-  id: number,
-  name: string,
-  storage: string,
-  price: number,
-  stock: number,
+  id: number;
+  name: string;
+  storage: string;
+  price: number;
+  stock: number;
 }
 
 const INVENTORY: inventoryData[] = [
-  {id: 1, name: 'Зубная щетка', storage: 'Хоругвино', price: 150, stock: 300},
-  {id: 2, name:'Мыло', storage: 'Хоругвино', price: 80, stock: 404},
-  {id: 1, name: 'Зубная щетка', storage: 'Тверь', price: 150, stock: 33},
-  {id: 3, name: 'Мазь для лица', storage: 'Тверь', price: 342, stock: 112},
-  {id: 2, name: 'Мыло', storage: 'Воронеж', price: 80, stock: 1099},
-  {id: 1, name: 'Зубная щетка', storage: 'Воронеж', price: 150, stock: 678},
-  {id: 1, name: 'Зубная щетка', storage: 'Хоругвино', price: 150, stock: 300},
-  {id: 2, name:'Мыло', storage: 'Хоругвино', price: 80, stock: 404},
-  {id: 1, name: 'Зубная щетка', storage: 'Тверь', price: 150, stock: 33},
-  {id: 3, name: 'Мазь для лица', storage: 'Тверь', price: 342, stock: 112},
-  {id: 2, name: 'Мыло', storage: 'Воронеж', price: 80, stock: 1099},
-  {id: 1, name: 'Зубная щетка', storage: 'Воронеж', price: 150, stock: 678},
-  {id: 1, name: 'Зубная щетка', storage: 'Хоругвино', price: 150, stock: 300},
-  {id: 2, name:'Мыло', storage: 'Хоругвино', price: 80, stock: 404},
-  {id: 1, name: 'Зубная щетка', storage: 'Тверь', price: 150, stock: 33},
-  {id: 3, name: 'Мазь для лица', storage: 'Тверь', price: 342, stock: 112},
-  {id: 2, name: 'Мыло', storage: 'Воронеж', price: 80, stock: 1099},
-  {id: 1, name: 'Зубная щетка', storage: 'Воронеж', price: 150, stock: 678},
-  {id: 1, name: 'Зубная щетка', storage: 'Хоругвино', price: 150, stock: 300},
-  {id: 2, name:'Мыло', storage: 'Хоругвино', price: 80, stock: 404},
-  {id: 1, name: 'Зубная щетка', storage: 'Тверь', price: 150, stock: 33},
-  {id: 3, name: 'Мазь для лица', storage: 'Тверь', price: 342, stock: 112},
-  {id: 2, name: 'Мыло', storage: 'Воронеж', price: 80, stock: 1099},
-  {id: 1, name: 'Зубная щетка', storage: 'Воронеж', price: 150, stock: 678},
+  { id: 1, name: 'Зубная щетка', storage: 'Хоругвино', price: 150, stock: 300 },
+  { id: 2, name: 'Мыло', storage: 'Хоругвино', price: 80, stock: 404 },
+  { id: 1, name: 'Зубная щетка', storage: 'Тверь', price: 150, stock: 33 },
+  { id: 3, name: 'Мазь для лица', storage: 'Тверь', price: 342, stock: 112 },
+  { id: 2, name: 'Мыло', storage: 'Воронеж', price: 80, stock: 1099 },
+  { id: 1, name: 'Зубная щетка', storage: 'Воронеж', price: 150, stock: 678 },
+  { id: 1, name: 'Зубная щетка', storage: 'Хоругвино', price: 150, stock: 300 },
+  { id: 2, name: 'Мыло', storage: 'Хоругвино', price: 80, stock: 404 },
+  { id: 1, name: 'Зубная щетка', storage: 'Тверь', price: 150, stock: 33 },
+  { id: 3, name: 'Мазь для лица', storage: 'Тверь', price: 342, stock: 112 },
+  { id: 2, name: 'Мыло', storage: 'Воронеж', price: 80, stock: 1099 },
+  { id: 1, name: 'Зубная щетка', storage: 'Воронеж', price: 150, stock: 678 },
+  { id: 1, name: 'Зубная щетка', storage: 'Хоругвино', price: 150, stock: 300 },
+  { id: 2, name: 'Мыло', storage: 'Хоругвино', price: 80, stock: 404 },
+  { id: 1, name: 'Зубная щетка', storage: 'Тверь', price: 150, stock: 33 },
+  { id: 3, name: 'Мазь для лица', storage: 'Тверь', price: 342, stock: 112 },
+  { id: 2, name: 'Мыло', storage: 'Воронеж', price: 80, stock: 1099 },
+  { id: 1, name: 'Зубная щетка', storage: 'Воронеж', price: 150, stock: 678 },
+  { id: 1, name: 'Зубная щетка', storage: 'Хоругвино', price: 150, stock: 300 },
+  { id: 2, name: 'Мыло', storage: 'Хоругвино', price: 80, stock: 404 },
+  { id: 1, name: 'Зубная щетка', storage: 'Тверь', price: 150, stock: 33 },
+  { id: 3, name: 'Мазь для лица', storage: 'Тверь', price: 342, stock: 112 },
+  { id: 2, name: 'Мыло', storage: 'Воронеж', price: 80, stock: 1099 },
+  { id: 1, name: 'Зубная щетка', storage: 'Воронеж', price: 150, stock: 678 },
 ];
 @Component({
   selector: 'app-inventory',
   templateUrl: './inventory.component.html',
-  styleUrls: ['./inventory.component.scss']
+  styleUrls: ['./inventory.component.scss'],
 })
-
 export class InventoryComponent implements AfterViewInit {
-  displayedColumns: string[] = ['id','name', 'storage', 'price', 'stock']
-  dataSource = new MatTableDataSource<inventoryData>(INVENTORY)
-  
+  displayedColumns: string[] = ['id', 'name', 'storage', 'price', 'stock'];
+  dataSource = new MatTableDataSource<inventoryData>(INVENTORY);
+
   constructor(private _liveAnnouncer: LiveAnnouncer) {}
 
   @ViewChild('tableSort') tableSort = new MatSort();
@@ -59,20 +58,18 @@ export class InventoryComponent implements AfterViewInit {
 
   announceSortChange(sortState: Sort | any) {
     if (sortState.direction) {
-      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`)
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
     } else {
-      this._liveAnnouncer.announce(`Soring cleared`)
+      this._liveAnnouncer.announce(`Soring cleared`);
     }
   }
-  sortedTable: inventoryData[] = INVENTORY
-  filterByStorageSelected: string = 'all'
-  handleFilterByStorageChange(): void {
-    this.sortedTable = INVENTORY
-    this.filterByStorageSelected !== 'all' 
-    ?
-    this.sortedTable = this.sortedTable.filter((item: inventoryData) => item.storage === this.filterByStorageSelected)
-    : 
-    this.sortedTable = INVENTORY
-    this.dataSource = new MatTableDataSource<inventoryData>(this.sortedTable)
+  sortedTable: inventoryData[] = INVENTORY;
+  storageFilter: string = 'all';
+  handleStorageFilterChange(): void {
+    this.sortedTable = INVENTORY;
+    this.sortedTable = this.sortedTable.filter((item: inventoryData) =>
+      this.storageFilter !== 'all' ? item.storage === this.storageFilter : item
+    );
+    this.dataSource = new MatTableDataSource<inventoryData>(this.sortedTable);
   }
 }
