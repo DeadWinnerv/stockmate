c6c4125952c171c4247418f283c4d5bf989e542b
DeadWinnerv <vitwinner@ya.ru>
2023-05-01 20:17:39 +0300

    storages layout temp changes

--- src/app/components/storages/storages.component.ts
+++ src/app/components/storages/storages.component.ts
@@ -1,25 +1,53 @@
 import { Component } from '@angular/core';
 import { Storage } from 'src/app/models/storage';
-
 @Component({
   selector: 'app-storages',
   templateUrl: './storages.component.html',
   styleUrls: ['./storages.component.scss'],
 })
 export class StoragesComponent {
-  panelOpenState: boolean = closed; 
-  addStorageWindowOpen: boolean = false;
+
+  activeStorage: number | undefined = undefined
+
   storages: Storage[] = [
     {
-      id: 1,
+      id: 16352,
       name: 'Склад 1',
       adress: 'г. Воронеж, ул.Александровская, д.6/1',
       schedule: 'full-time',
     },
     {
-      id: 2,
+      id: 12363,
       name: 'Склад 2',
       adress: 'г. Воронеж, ул. Федора Тютчева, д.99а',
+      schedule: 'weekdays',
+      phone: '+79008005544',
+      email: 'email@mail.com'
+    },
+    {
+      id: 12353,
+      name: 'Склад 3',
+      adress: 'г. Воронеж, ул., д.6/1',
+      schedule: 'full-time',
+    },
+    {
+      id: 52434,
+      name: 'Склад 4',
+      adress: 'г. Воронеж, ул. Федора, д.99а',
+      schedule: 'weekdays',
+      phone: '+79008005544',
+      email: 'email@mail.com'
+    },
+    {
+      id: 12312,
+      name: 'Склад 5',
+      adress: ' Harum, repellat veniam! Reprehenderit debitis deserunt animi!',
+      schedule: 'full-time',
+    },
+    {
+      id: 21231,
+      name: 'Склад 6',
+      adress: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
       schedule: 'weekdays',
       phone: '+79008005544',
       email: 'email@mail.com'
