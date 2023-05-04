9ac3a4ad7125e22c5c700c2916356fa1aaafa93e
DeadWinnerv <vitwinner@ya.ru>
2023-05-04 19:06:25 +0300

    products page layout added

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PRODUCTS, IProduct } from 'src/app/constants/PRODUCTS';
 
 @Component({
   selector: 'app-products',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent {
+  products: IProduct[] = PRODUCTS
+  activeTab: 'filter' | 'addNew' = 'addNew'
 
+  handleTabChange(tabName: 'filter' | 'addNew') {
+    this.activeTab = tabName
+  }
 }
---
+++ src/app/constants/PRODUCTS.ts
@@ -0,0 +1,26 @@
+export interface IProduct {
+	id: string,
+	name: string,
+	price: number
+}
+
+export const PRODUCTS: IProduct[] = [
+	{ id: "p1", name: "Телефон", price: 799 },
+  { id: "p2", name: "Ноутбук", price: 1499 },
+  { id: "p3", name: "Клавиатура", price: 79 },
+  { id: "p4", name: "Мышь", price: 39 },
+  { id: "p5", name: "Наушники", price: 199 },
+  { id: "p6", name: "Монитор", price: 399 },
+  { id: "p7", name: "Камера", price: 299 },
+  { id: "p8", name: "Видеоигры", price: 59 },
+  { id: "p9", name: "Микрофон", price: 129 },
+  { id: "p10", name: "Смарт-часы", price: 299 },
+  { id: "p11", name: "Портативный зарядный устройство", price: 29 },
+  { id: "p12", name: "Сотовый модем", price: 149 },
+  { id: "p13", name: "Фотоаппарат", price: 399 },
+  { id: "p14", name: "Компьютерные динамики", price: 79 },
+  { id: "p15", name: "USB-флешка", price: 19 },
+  { id: "p16", name: "Жесткий диск", price: 99 },
+  { id: "p17", name: "Принтер", price: 199 },
+  { id: "p18", name: "Wi-Fi роутер", price: 79 }
+]
