a272afb85c97b1b08afedb8b3f7891b1b3891742
DeadWinnerv <vitwinner@ya.ru>
2023-05-09 23:30:46 +0300

    ngx-charts added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSortModule } from '@angular/material/sort';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { NgxChartsModule }from '@swimlane/ngx-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -66,7 +67,8 @@ import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
     MatProgressSpinnerModule,
     MatSortModule,
     MatAutocompleteModule,
-    SweetAlert2Module
+    SweetAlert2Module,
+    NgxChartsModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
