7c042a804173bbb416d8791c827bbe459f2bca76
DeadWinnerv <vitwinner@ya.ru>
2023-05-17 22:41:02 +0300

    order adding filters for storages and products

--- src/app/components/orders/orders.component.ts
+++ src/app/components/orders/orders.component.ts
@@ -1,17 +1,27 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { IOrder } from 'src/app/models/order';
 import { SortTableDirective } from 'src/directives/sortTable.directive';
 import { OrdersService } from 'src/app/services/orders.service';
+import { ProductService } from 'src/app/services/product.service';
+import { IProduct } from 'src/app/models/product';
+import { StorageService } from 'src/app/services/storage.service';
+import { Observable, map, startWith } from 'rxjs';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.scss'],
 })
-export class OrdersComponent implements OnInit {
+export class OrdersComponent implements OnInit, OnDestroy {
   isLoading: boolean = false;
   ordersList: IOrder[];
-  protected activeFilter: 'created' | 'paid' | 'delievering' | 'acceptence' | 'completed' = 'created'
+  protected activeFilter:
+    | 'created'
+    | 'paid'
+    | 'delievering'
+    | 'acceptence'
+    | 'completed' = 'created';
   protected displayedColumns: string[] = [];
   protected sortingColumn: {
     column: number | 'total' | '';
@@ -21,14 +31,45 @@ export class OrdersComponent implements OnInit {
   };
   protected shownColumn: number | 'total' | '';
   protected shownDetails: number | undefined;
-  protected isAddingNewOrder = false;
   protected isTableEmpty = false;
+  protected productsList: IProduct[] = [];
+  protected isAddingNewOrder = this.productsList[0] ? false : true;
+  protected storageOptions: string[];
+  protected filteredStorageOptions: Observable<string[]>
+  protected storageInput = new FormControl('', Validators.required)
+  protected filteredProductOptions: Observable<string[]>
+  protected productsFilter = new FormControl('')
+
 
   @ViewChild(SortTableDirective) sortTableDirective: SortTableDirective;
 
-  constructor(private ordersService: OrdersService) { }
+  constructor(
+    private ordersService: OrdersService,
+    private productsService: ProductService,
+    private storageService: StorageService
+  ) {}
   ngOnInit(): void {
-    this.loadOrdersList()
+    this.loadProductsList();
+    this.loadOrdersList();
+    this.loadStorages();
+    this.filteredStorageOptions = this.storageInput.valueChanges.pipe(
+      startWith(''),
+      map((value) => {
+        const name = value
+        return name ? this._filterOptions(name as string, this.storageOptions) : this.storageOptions;
+      })
+    )
+    this.filteredProductOptions = this.productsFilter.valueChanges.pipe(
+      startWith(''),
+      map((value) => {
+        const name = value
+        return name !== '' && name ? this._filterOptions(name as string, this.productsList.map(item => item.name)) : this.productsList.map(item => item.name);
+      })
+    )
+  }
+
+  ngOnDestroy(): void {
+    
   }
 
   loadOrdersList() {
@@ -45,41 +86,70 @@ export class OrdersComponent implements OnInit {
                   item.toLowerCase().includes(column)
                 )
             ),
-          ]
+          ];
         } else {
           this.isTableEmpty = true;
         }
-        this.isLoading = false
+        this.isLoading = false;
       },
-      error: (err) => console.log(err)
+      error: (err) => console.error(err),
+    });
+  }
+
+  loadProductsList() {
+    this.productsService.getProducts().subscribe({
+      next: (res) => {
+        this.productsList = res;
+        this.filteredProductOptions.subscribe
+      },
+      error: (err) => {
+        console.error(err);
+        
+      }
+    })
+  }
+
+  loadStorages() {
+    this.storageService.getStorages().subscribe({
+      next: (res) => {
+        this.storageOptions = res.map(storage => storage.name)
+      },
+      error: (err) => {
+        console.error(err);
+      }
     })
   }
 
   handleTableHeaderHover(i: number | 'total' | '') {
-    this.shownColumn = i
+    this.shownColumn = i;
   }
 
   handleSortDirection(i: number) {
-    if(this.sortingColumn.column !== i) {
+    if (this.sortingColumn.column !== i) {
       this.sortingColumn.column = i;
-      this.sortingColumn.direction = undefined
-    } 
+      this.sortingColumn.direction = undefined;
+    }
     if (!this.sortingColumn.direction) {
-      this.sortingColumn.direction = 'asc'
-      this.ordersList = this.sortTableDirective.getSortedTable()
+      this.sortingColumn.direction = 'asc';
+      this.ordersList = this.sortTableDirective.getSortedTable();
     } else if (this.sortingColumn.direction === 'asc') {
-      this.sortingColumn.direction = 'desc'
-      this.ordersList = this.sortTableDirective.getSortedTable()
+      this.sortingColumn.direction = 'desc';
+      this.ordersList = this.sortTableDirective.getSortedTable();
     } else {
-      this.shownColumn = ''
-      this.sortingColumn.column = ''
-      this.sortingColumn.direction = undefined
+      this.shownColumn = '';
+      this.sortingColumn.column = '';
+      this.sortingColumn.direction = undefined;
     }
   }
 
   showDetails(i: number) {
     this.shownDetails === i
-    ? this.shownDetails = undefined
-    : this.shownDetails = i
+      ? (this.shownDetails = undefined)
+      : (this.shownDetails = i);
+  }
+
+  private _filterOptions(name: string, options: string[]) {
+    const filterValue = name.toLowerCase();
+    return options.filter(option => option.toLowerCase().includes(filterValue));
   }
 }
